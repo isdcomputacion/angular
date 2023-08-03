173671407da8596e82b01038ececf20c3870346f
isdcomputacion <isdcomputacion@gmail.com>
2023-08-03 20:56:21 -0300

    main

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'webfinal'`, () => {
+  it(`should have as title 'FINAL'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('webfinal');
+    expect(app.title).toEqual('FINAL');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('webfinal app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('FINAL app is running!');
   });
 });
